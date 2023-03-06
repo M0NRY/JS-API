cc9fa278147497fdecb812ba30eb70e8fe57ffad
Emmanuel Monroy <112371036+M0NRY@users.noreply.github.com>
2023-03-05 23:36:41 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,76 @@
+function Leer() {
+    const team = document.getElementById("input").value;
+    //obtain an apikey on this web
+    //http://www.omdbapi.com/apikey.aspx
+    const key = "89393bf57beb36c759a9a0f59a4d208dd229e5b647f216ebeb63d61ba3c7bb0e"; 
+    const api_url=  `https://apiv3.apifootball.com/?s=${team}&apikey=${key}`
+    buscar1(api_url);                                                            
+    
+}
+
+function buscar1(api_url){
+
+    fetch(api_url)
+      .then(data => {
+        return data.json()
+      }).then(resultado=>{
+            console.log(resultado);
+
+            const {Search=[]} = resultado;
+            
+            console.log(Search);
+            document.getElementById("lista").innerHTML='';
+
+            Search.map((p)=>{
+                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+                         <img width='100%' src=${p.Poster} alt="No disponible"></img>
+            </div>`;
+            })
+      });
+
+
+}
+
+const buscar2=async(api_url)=>{
+
+    const data= await fetch(api_url);
+    const respuesta= await data.json();
+    const Search = await respuesta.Search;
+
+    console.log(Search);
+
+    if(Search!=null)
+    {   
+        document.getElementById("lista").innerHTML='';
+        Search.map((p)=>{
+                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+                    <img width='100%' src=${p.Poster} alt="No dsiponible"></img></div>`;
+        })
+
+    }
+
+}    
+
+     
+const buscar3=async(api_url)=>{
+
+    const respuesta= await axios(api_url);
+    const Search = await respuesta.data.Search;
+    console.log(respuesta.data);
+    
+    console.log(Search);
+
+    
+    if(Search!=null)
+    {
+        document.getElementById("lista").innerHTML='';
+        
+        Search.map((p)=>{
+                document.getElementById("lista").innerHTML+=`<div style="margin-top:10px;">
+                    <img width='100%' src=${p.Poster} alt="No dsiponible"></img></div>`;
+        })
+
+    }
+
+}    
 
